ff03672b5e01e80455d0c5e43f53f22ddb59228c
amarall06 <amarall06@users.noreply.github.com>
2024-07-06 09:13:33 -0300

    Exercicio 11 condição simples

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { // Condição simples!
+    console.log('Voce Ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando o cinto de segurança')

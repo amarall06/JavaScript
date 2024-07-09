1b21c42a3f02f730a0cc111ba9e51ca4a5e61f02
amarall06 <amarall06@users.noreply.github.com>
2024-07-08 21:03:09 -0300

    att

---
+++ testes/node_50f54f3a45191.tmp.js
@@ -0,0 +1,7 @@
+var idade = 25
+console.log('Voce tem ${idade} anos')
+if (idade < 20 ){
+    console.log('Aprovado!')
+} else {
+    console.log ('Reprovado!')
+}
---
+++ testes/node_c9296f7c45bfd.tmp.js
@@ -0,0 +1,1 @@
+console.log(`Voce tem ${idade} anos`)

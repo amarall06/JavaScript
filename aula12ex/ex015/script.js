ed707422a37e65043b85577773769bc060d286a2
amarall06 <amarall06@users.noreply.github.com>
2024-07-10 14:07:35 -0300

    aa

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,21 +3,25 @@ function verificar() {
    var ano = data.getFullYear()
    var fano = document.getElementById('txtano')
    var res = document.querySelector('div#res')
-   if (fano.value.length == 0 || Number(fano.value) > ano){
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
-   }else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var gênero = ''
-        if (fsex[0].Checked) {
-            gênero = 'Homem'
-         } else if (fsex[1].Checked) {
-            gênero = 'Mulher'
+   if (fano.ariaValueMax.length == 0 || Number(fano.value) > ano) {
+      window.alert('[ERRO] Verifique os dados e tente novamente!')
+   }else  {
+      var fsex = document.getElementsByName('radsex')
+      var idade = ano - Number(fano.ariaValueMax)
+      var gênero = ''
+      var img = document.createElement('img')
+      img.setAttribute('id', 'foto')
+      if (fsex[0].ariaChecked) {
+         gênero = 'Homem'
+         if (idade >=0 && idade <10){
+            //Criança
+         }else if (dade >= 10 && idade <21){
+
          }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
-
-        
+      }else if (fsex[1].ariaChecked) {
+         gênero = 'Mulher'
+      }
+      res.style.textAlign = 'center'
+      res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
    }
-
 }

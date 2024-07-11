e5493d3877d0f31350588ce9392d047da8997bae
amarall06 <amarall06@users.noreply.github.com>
2024-07-11 09:27:38 -0300

    Repetição(parte 1)

---
+++ aula13/ambiente.js
@@ -0,0 +1,31 @@
+var c = 1
+while (c <= 5 ){ //contar menor do 6(5), adicionando = ele vai contar ate 6
+    console.log(`Passo ${c}`)
+    c++ //c = c +1
+}
+
+/* OUTRO TIPO DE REPETIÇAO:
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 5)
+    
+    
+*/
+
+
+
+
+
+/*
+ NÃO FAZER DESSE JEITO NUNCA!!
+ 
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+
+*/

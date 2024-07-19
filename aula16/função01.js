8319523ecbeaed3becae054bae9cf32f11bacfc1
amarall06 <amarall06@users.noreply.github.com>
2024-07-19 13:56:44 -0300

    funções em JS

---
+++ aula16/função01.js
@@ -0,0 +1,10 @@
+function parimpar(n) { //n = parametro
+    if(n%2 ==0) {
+        return 'Par!'
+    } else {        // todo o if é açao
+        return 'Impar!'
+    }
+}
+
+console.log(parimpar(5))
+//chamada

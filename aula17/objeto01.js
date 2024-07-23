258855e20e115166ccb5d4baa5bf1a6a2ead9fed
amarall06 <amarall06@users.noreply.github.com>
2024-07-23 15:34:01 -0300

    um pouco de objt

---
+++ aula17/objeto01.js
@@ -0,0 +1,9 @@
+let amigo = {nome:'Felipe',
+Sexo: 'M',
+peso: 75.5,
+engordar(p=0){
+    console.log('Engordou')
+    this.peso += p
+}}
+    amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}kg`)
